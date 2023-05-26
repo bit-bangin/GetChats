1b94af635d9e0f53a3aaf5bc5e46b2af4e4528b7
Emily <94324680+bit-bangin@users.noreply.github.com>
2023-05-26 02:48:05 +0000

    Improved error handling to including logging.

--- APItalk.py
+++ APItalk.py
@@ -1,10 +1,32 @@
+"""
+This module interacts with the OpenAI API to retrieve data based upon a given query.
+
+"""
+
 import time
+import logging
 import requests
-from requests.exceptions import Timeout
+from requests.exceptions import RequestException
+
+# Configure the logging module
+logging.basicConfig(level=logging.info)
+logger = logging.getLogger(__name__)
 
 def apiTalk(api_key, query):
     """
-    Function to 
+    Sends a POST request to the OpenAI API with the given query, using the provided API key for authentication. 
+    
+    Handles rate limits by sleeping for 60 seconds upon receipt of a 429 status code - then retries request.
+
+    Arguments
+        api_key (str): The API key for authentication.
+        query (str): The query to send to the OpenAI API.
+
+    Returns:
+        dict: Data returned by the API, parsed from JSON into a dictionary.
+
+    Raises:
+        HTTPError: If response w/error status code received. 
     """
     url = "https://api.openai.com/v1/engines/davinci-codex/completions"
 
@@ -23,19 +45,22 @@ def apiTalk(api_key, query):
             response = requests.post(url, headers = headers, json = data, timeout = 10)
 
             if response.status_code == 200:
-                # Request was successful, return results
+                # Request was successful, return the results
                 return response.json()
             elif response.status_code == 429:
-                # We have hit the rate limit, sleep then try again
+                # We've encountered the rate limit, napping before trying again.
+                logger.info("We have encountered the rate limit. Taking a quick nap.")
                 time.sleep(60)
                 continue
             else:
                 # Some other error has occurred
                 response.raise_for_status()
-        except Timeout:
-            print("Request has timed out, sleeping for a time - then trying again.")
+        except RequestException as e:
+            # Catch any requests exceptions.
+            logger.error(f"A requests exception has occurred: {e}")
             time.sleep(1)
             continue
         except Exception as e:
-            print(f"An error ocurred: {e}")
+            # Catch any other unforeseen exceptions.
+            logger.error(f"An error ocurred: {e}")
             break
