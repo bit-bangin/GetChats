33ee8c782a465c6c0ef46d13b4b3b5eb2660e00a
Emily <94324680+bit-bangin@users.noreply.github.com>
2023-05-26 02:23:48 +0000

    Updated to include doc string

--- APItalk.py
+++ APItalk.py
@@ -3,6 +3,9 @@ import requests
 from requests.exceptions import Timeout
 
 def apiTalk(api_key, query):
+    """
+    Function to 
+    """
     url = "https://api.openai.com/v1/engines/davinci-codex/completions"
 
     headers = {
@@ -20,14 +23,14 @@ def apiTalk(api_key, query):
             response = requests.post(url, headers = headers, json = data, timeout = 10)
 
             if response.status_code == 200:
-                # Request was successful, return results. 
+                # Request was successful, return results
                 return response.json()
             elif response.status_code == 429:
-                # We have hit the rate limit, sleep then try again.
+                # We have hit the rate limit, sleep then try again
                 time.sleep(60)
                 continue
             else:
-                # Some other error has occurred.
+                # Some other error has occurred
                 response.raise_for_status()
         except Timeout:
             print("Request has timed out, sleeping for a time - then trying again.")
